9da843066e5e9cf3f78523f84274e677b33bda7c
Jack Cullen <jpcullen20@gmail.com>
2020-03-25 09:41:05 -0600

    Add total_number_of_products and is_full? methods with tests

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -11,4 +11,14 @@ class ShoppingCart
   def add_product(product)
     @products << product
   end
+
+  def total_number_of_products
+    @products.reduce(0) do |total, product|
+      total += product.quantity
+    end
+  end
+
+  def is_full?
+    total_number_of_products >= @capacity
+  end
 end
--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -9,6 +9,8 @@ class ShoppingCartTest < Minitest::Test
     @cart = ShoppingCart.new("King Soopers", "30items")
     @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
     @product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    @product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    @product4 = Product.new(:produce, 'apples', 0.99, '20')
   end
 
   def test_it_exists
@@ -32,6 +34,34 @@ class ShoppingCartTest < Minitest::Test
     @cart.add_product(@product2)
 
     assert_equal [@product1, @product2], @cart.products
+  end
+
+  def test_total_number_of_products
+    @cart.add_product(@product1)
+    @cart.add_product(@product2)
+    @cart.add_product(@product3)
+
+    assert_equal 13, @cart.total_number_of_products
+  end
+
+  def test_is_full_false_when_under_capacity
+    @cart.add_product(@product1)
+    @cart.add_product(@product2)
+    @cart.add_product(@product3)
+
+    assert_equal false, @cart.is_full?
+  end
+
+  def test_is_full_true_when_at_capacity
+    @cart.add_product(@product1)
+    @cart.add_product(@product2)
+    @cart.add_product(@product3)
+
+    assert_equal false, @cart.is_full?
+
+    @cart.add_product(@product4)
+
+    assert_equal true, @cart.is_full?
   end
 
 end
