9db2aefc2df3530024ffcbbc18ea5c41f751ff33
Jack Cullen <jpcullen20@gmail.com>
2020-03-25 09:25:31 -0600

    Add shopping_cart class and test is has readable attributes

---
+++ test/shopping_cart_test.rb
@@ -0,0 +1,28 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/product'
+require './lib/shopping_cart'
+
+class ShoppingCartTest < Minitest::Test
+
+  def setup
+    @cart = ShoppingCart.new("King Soopers", "30items")
+  end
+
+  def test_it_exists
+    assert_instance_of ShoppingCart, @cart
+  end
+
+  def test_it_has_a_name
+    assert_equal "King Soopers", @cart.name
+  end
+
+  def test_it_has_a_capacity
+    assert_equal 30, @cart.capacity
+  end
+
+  def test_products_empty_by_default
+    assert_equal [], @cart.products
+  end
+
+end
